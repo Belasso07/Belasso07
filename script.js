5c417047c886c260e694ef8e422232f889a09f2b
Belasso07 <wallacecrystian7@gmail.com>
2025-01-02 00:47:16 -0300

    subi os arquivos do belasso07

---
+++ script.js
@@ -0,0 +1,15 @@
+function adicione() {
+    alert('Me adicione na PSN! Meu nick é: Belasso07')
+}
+
+function email() {
+    alert('Me mande um email! Email: wallacecrystian7@gmail.com')
+}
+
+function nintendo() {
+    alert('Me adicione na Nintendo! Meu nick é: Belasso07')
+}
+
+function internet() {
+    alert('Nick(Jogos e Internet) = Belasso07')
+}
